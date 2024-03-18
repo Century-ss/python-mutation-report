b033c69b85946d19a88cccede66dd47f9c41ca9d
Century-ss <hgfc7543ggfdch@gmail.com>
2024-03-18 17:17:54 +0900

    👍 reshape finish

--- scripts/reshape_result_for_PR_comment.py
+++ scripts/reshape_result_for_PR_comment.py
@@ -1,21 +1,16 @@
-with open("results/run_result.txt", "r") as f:
+import xml.etree.ElementTree as ET
+
+import bs4
+
+with open("results/run.txt", "r") as f:
     run_result_rows = [s.rstrip() for s in f.readlines()]
 
-with open("results/show_all_result.txt", "r") as f:
-    show_all_result_rows = f.readlines()
-
+""" Make main summary text
+"""
 final_run_result = run_result_rows[-1].split("  ")[1:]
-number_of_legend = len(final_run_result)
-summary_text = "Mutation Test Result　　" + "　　".join(final_run_result)
+main_summary = "Mutation Test Result　　" + "　　".join(final_run_result)
 
-# TODO: no need to check if the legend is in the file, just add it?
-# LEGEND_MARKER = "Legend for output:"
-# if LEGEND_MARKER in run_result_rows:
-#     legend_index = run_result_rows.index(LEGEND_MARKER)
-#     legend_description = "\n".join(
-#         run_result_rows[legend_index : legend_index + number_of_legend + 1]
-#     )
-# else:
+
 legend_description = (
     "Legend for output:\n"
     + "🎉 Killed mutants.   The goal is for everything to end up in this bucket.\n"
@@ -25,18 +20,67 @@ legend_description = (
     + "🔇 Skipped.          Skipped.\n"
 )
 
-mutant_codes = "".join(show_all_result_rows[7:])
 
-contents = (
+""" Make sub summary and mutants
+"""
+tree = ET.parse("results/junit.xml")
+root = tree.getroot()
+line_number_dict = {
+    testcase.attrib["name"]: testcase.attrib["line"] for testcase in root.iter("testcase")
+}  # {"Mutant #3": "3"} Key: mutant id(ex.3), Value: line number
+
+soup = bs4.BeautifulSoup(open("html/index.html"), "html.parser")
+file_list = [{"href": a_tag["href"], "file_name": a_tag.get_text()} for a_tag in soup.find_all("a")]
+
+for a_tag in soup.find_all("a"):
+    a_tag.replace_with(a_tag.get_text())
+sub_summary = str(soup)
+
+mutants_per_file = ""
+for file in file_list:
+    content = bs4.BeautifulSoup(open(f"html/{file['href']}"), "html.parser")
+    content_children = list(content.stripped_strings)
+    code_blocks = ""
+    for index, element in enumerate(content_children):
+        if index == 0:
+            continue
+        if element in ["Survived", "Timeouts", "Suspicious"]:
+            element = f"## {element}"
+
+        if element.startswith("Mutant ") and element[7:].isdigit():
+            mutant_id = int(element[7:])
+            line_number = line_number_dict[f"Mutant #{mutant_id}"]
+            element = f"### Mutant id:{mutant_id}　Line:{line_number}\n```"
+
+        if (
+            content_children[index - 1].startswith("Mutant ")
+            and content_children[index - 1][7:].isdigit()
+        ):
+            element = f"{element}\n```"
+
+        code_blocks += element + "\n"
+
+    mutants_per_file += (
+        "<details><summary>" + file["file_name"] + "</summary>\n\n" + code_blocks + "</details>\n\n"
+    )
+
+
+PR_comment = (
     "<details><summary>"
-    + summary_text
+    + main_summary
     + "</summary>\n\n"
-    + "```txt\n"
+    + "<br>\n\n"
     + legend_description
     + "\n\n"
-    + mutant_codes
+    + sub_summary
+    + "※ ⏰Timeout, 🤔Suspicious and 🔇Skipped are not shown in the table."
+    + "\n<br>\n"
+    + "\n<br>\n"
+    + "※ 🔇Skipped are not shown in the list of mutants"
+    + "\n\n"
+    + mutants_per_file
     + "</details>"
 )
 
 with open("PR_comment.txt", "w") as f:
-    f.write(contents)
+    f.write(PR_comment)
