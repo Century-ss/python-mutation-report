10f844b15c01cd3bc9602a85da03d3f0e6f89e58
Century-ss <73206421+Century-ss@users.noreply.github.com>
2024-03-29 09:25:27 +0900

    👷 support execute test outside src pattern

--- scripts/make_command_of_run.py
+++ scripts/make_command_of_run.py
@@ -5,13 +5,16 @@ src_directory = os.environ.get("SRC_DIRECTORY")
 test_directory = os.environ.get("TEST_DIRECTORY")
 where_to_run_test = os.environ.get("WHERE_TO_RUN_TEST", ".")
 composite_action_path = os.environ.get("COMPOSITE_ACTION_PATH")
+workspace_path = os.environ.get("WORKSPACE_PATH")
 
 if src_directory is None:
     raise ValueError("src_directory is not set.")
 if test_directory is None:
     raise ValueError("test_directory is not set.")
 if composite_action_path is None:
-    raise ValueError("actions_path of github context is not set.")
+    raise ValueError("actions_path of github context is not found.")
+if workspace_path is None:
+    raise ValueError("workspace of github context is not found.")
 
 src_directory = src_directory.removeprefix("./")
 test_directory = test_directory.removeprefix("./")
@@ -24,7 +27,7 @@ with open(os.path.join(temporary_directory, "PR_diff_files.txt"), "r") as f:
     changed_file_paths = [s.rstrip() for s in f.readlines()]
 
 changed_py_files_except_init = [
-    {"name": os.path.basename(file_path), "path": file_path}
+    {"name": os.path.basename(file_path), "relative_path": file_path}
     for file_path in changed_file_paths
     if file_path.endswith(".py") and not file_path.endswith("__init__.py")
 ]
@@ -32,21 +35,21 @@ changed_py_files_except_init = [
 changed_src_files = [
     file
     for file in changed_py_files_except_init
-    if file["path"].startswith(src_directory) and not file["name"].startswith("test_")
+    if file["relative_path"].startswith(src_directory) and not file["name"].startswith("test_")
 ]
 changed_test_files = [
     file
     for file in changed_py_files_except_init
-    if file["path"].startswith(test_directory) and file["name"].startswith("test_")
+    if file["relative_path"].startswith(test_directory) and file["name"].startswith("test_")
 ]
 
 all_src_files = [
-    {"name": os.path.basename(file_path), "path": file_path}
+    {"name": os.path.basename(file_path), "relative_path": file_path}
     for file_path in glob.glob(os.path.join(src_directory, "**/*.py"), recursive=True)
     if not os.path.basename(file_path).startswith("test_")
 ]
 all_test_files = [
-    {"name": os.path.basename(file_path), "path": file_path}
+    {"name": os.path.basename(file_path), "relative_path": file_path}
     for file_path in glob.glob(os.path.join(test_directory, "**/*.py"), recursive=True)
     if os.path.basename(file_path).startswith("test_")
 ]
@@ -56,41 +59,49 @@ file_paths_to_run_test = []
 
 for changed_src_file in changed_src_files:
     test_file_paths_match_to_src_filename = [
-        all_test_file["path"]
+        all_test_file["relative_path"]
         for all_test_file in all_test_files
         if all_test_file["name"] == f"test_{changed_src_file['name']}"
     ]
     if len(test_file_paths_match_to_src_filename) > 0:
-        file_paths_to_mutate.append(changed_src_file["path"])
+        file_paths_to_mutate.append(changed_src_file["relative_path"])
         file_paths_to_run_test.extend(test_file_paths_match_to_src_filename)
 
 for changed_test_file in changed_test_files:
     src_file_paths_match_to_test_filename = [
-        all_src_file["path"]
+        all_src_file["relative_path"]
         for all_src_file in all_src_files
         if all_src_file["name"] == changed_test_file["name"].removeprefix("test_")
     ]
     if len(src_file_paths_match_to_test_filename) > 0:
-        file_paths_to_run_test.append(changed_test_file["path"])
+        file_paths_to_run_test.append(changed_test_file["relative_path"])
         file_paths_to_mutate.extend(src_file_paths_match_to_test_filename)
 
 if len(file_paths_to_mutate) == 0 or len(file_paths_to_run_test) == 0:
     raise ValueError("Not found files to mutate or tests.")
 
-if where_to_run_test != ".":
-    file_paths_to_mutate = [
-        file_path.removeprefix(where_to_run_test).removeprefix("/")
-        for file_path in file_paths_to_mutate
-    ]
-    file_paths_to_run_test = [
-        file_path.removeprefix(where_to_run_test).removeprefix("/")
-        for file_path in file_paths_to_run_test
-    ]
+# if where_to_run_test != ".":
+#     file_paths_to_mutate = [
+#         file_path.removeprefix(where_to_run_test).removeprefix("/")
+#         for file_path in file_paths_to_mutate
+#     ]
+#     file_paths_to_run_test = [
+#         file_path.removeprefix(where_to_run_test).removeprefix("/")
+#         for file_path in file_paths_to_run_test
+#     ]
+
+# NOTE: Use a path from root for cases where the src is outside the path of the test to be run.
+absolute_file_paths_to_mutate = [
+    os.path.join(workspace_path, path) for path in file_paths_to_mutate
+]
+absolute_file_paths_to_run_test = [
+    os.path.join(workspace_path, path) for path in file_paths_to_run_test
+]
 
 command = (
-    f"mutmut run --paths-to-mutate {','.join(file_paths_to_mutate)}"
-    + f" --tests-dir {','.join(file_paths_to_run_test)}"
-    + f" --runner 'python -m pytest -x --assert=plain {' '.join(file_paths_to_run_test)}'"
+    f"mutmut run --paths-to-mutate {','.join(absolute_file_paths_to_mutate)}"
+    + f" --tests-dir {','.join(absolute_file_paths_to_run_test)}"
+    + f" --runner 'python -m pytest -x --assert=plain {' '.join(absolute_file_paths_to_run_test)}'"
     + f" > {temporary_directory}/run.txt"
 )
 
--- scripts/reshape_result_for_PR_comment.py
+++ scripts/reshape_result_for_PR_comment.py
@@ -5,12 +5,17 @@ import xml.etree.ElementTree as ET
 import bs4
 
 composite_action_path = os.environ.get("COMPOSITE_ACTION_PATH")
+workspace_path = os.environ.get("WORKSPACE_PATH")
+
 if composite_action_path is None:
-    raise ValueError("actions_path of github context is not set.")
+    raise ValueError("actions_path of github context is not found.")
+if workspace_path is None:
+    raise ValueError("workspace of github context is not found.")
 
-temporary_directory = os.path.join(composite_action_path, "temporary")
+TEMPORARY_DIRECTORY = os.path.join(composite_action_path, "temporary")
+PREFIX_TO_REMOVE = workspace_path + "/"
 
-with open(os.path.join(temporary_directory, "run.txt"), "r") as f:
+with open(os.path.join(TEMPORARY_DIRECTORY, "run.txt"), "r") as f:
     run_result_rows = [s.rstrip() for s in f.readlines()]
 
 """ Make main summary text
@@ -31,40 +36,45 @@ legend_description = (
 
 """ Make sub summary and mutants
 """
-tree = ET.parse(os.path.join(temporary_directory, "junit.xml"))
+soup = bs4.BeautifulSoup(open(os.path.join(TEMPORARY_DIRECTORY, "html/index.html")), "html.parser")
+files = [
+    {"href": a_tag["href"], "relative_path": a_tag.get_text().removeprefix(PREFIX_TO_REMOVE)}
+    for a_tag in soup.find_all("a")
+]
+
+for a_tag in soup.find_all("a"):
+    a_tag.replace_with(a_tag.get_text().removeprefix(PREFIX_TO_REMOVE))
+sub_summary = str(soup)
+
+with open(os.path.join(TEMPORARY_DIRECTORY, "mutmut-run.sh"), "r") as f:
+    run_command = f.read()
+matched_tests_dir = re.search(r"--tests-dir\s+([^ ]*)", run_command)
+if matched_tests_dir is not None:
+    matched_absolute_test_paths = matched_tests_dir.group(1).split(",")
+    matched_test_paths = [
+        path.removeprefix(PREFIX_TO_REMOVE) for path in matched_absolute_test_paths
+    ]
+    test_paths_to_show = "- " + "\n- ".join(matched_test_paths)
+else:
+    test_paths_to_show = "Not matched test directory"
+
+test_paths_of_sub_summary = (
+    "<details><summary>"
+    + "List of test used for mutation"
+    + "</summary>\n\n"
+    + test_paths_to_show
+    + "\n</details>\n\n"
+)
+
+tree = ET.parse(os.path.join(TEMPORARY_DIRECTORY, "junit.xml"))
 root = tree.getroot()
 line_number_dict = {
     testcase.attrib["name"]: testcase.attrib["line"] for testcase in root.iter("testcase")
 }  # {"Mutant #3": "3"} Key: mutant id(ex.3), Value: line number
 
-soup = bs4.BeautifulSoup(open(os.path.join(temporary_directory, "html/index.html")), "html.parser")
-file_list = [{"href": a_tag["href"], "file_name": a_tag.get_text()} for a_tag in soup.find_all("a")]
-
-for a_tag in soup.find_all("a"):
-    a_tag.replace_with(a_tag.get_text())
-sub_summary = str(soup)
-
-with open(os.path.join(temporary_directory, "mutmut-run.sh"), "r") as f:
-    run_command = f.read()
-test_files_matched = re.search(r"--tests-dir\s+([^ ]*)", run_command)
-if test_files_matched is not None:
-    test_files = "- " + "\n- ".join(test_files_matched.group(1).split(","))
-else:
-    test_files = "Not matched test directory"
-
-show_test_directories = (
-    "<details><summary>"
-    + "List of test used for mutation"
-    + "</summary>\n\n"
-    + test_files
-    + "\n</details>\n\n"
-)
-
 mutants_per_file = ""
-for file in file_list:
-    content = bs4.BeautifulSoup(
-        open(os.path.join(temporary_directory, f"html/{file['href']}")), "html.parser"
-    )
+for file in files:
+    content = bs4.BeautifulSoup(open(file["href"]), "html.parser")
     content_children = list(content.stripped_strings)
     code_blocks = ""
     for index, element in enumerate(content_children):
@@ -82,12 +92,17 @@ for file in file_list:
             content_children[index - 1].startswith("Mutant ")
             and content_children[index - 1][7:].isdigit()
         ):
-            element = f"{element}\n```"
+            code_block = element.replace(PREFIX_TO_REMOVE, "")
+            element = f"{code_block}\n```"
 
         code_blocks += element + "\n"
 
     mutants_per_file += (
-        "<details><summary>" + file["file_name"] + "</summary>\n\n" + code_blocks + "</details>\n\n"
+        "<details><summary>"
+        + file["relative_path"]
+        + "</summary>\n\n"
+        + code_blocks
+        + "</details>\n\n"
     )
 
 
@@ -100,7 +115,7 @@ PR_comment = (
     + "\n\n"
     + sub_summary
     + "\n\n"
-    + show_test_directories
+    + test_paths_of_sub_summary
     + "※ ⏰Timeout, 🤔Suspicious and 🔇Skipped are not shown in the table."
     + "\n<br>\n"
     + "※ 🔇Skipped are not shown in the list of mutants"
