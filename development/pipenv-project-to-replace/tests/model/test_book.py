f1972966ede9619d58bde639bcac38f46f51a14a
Century-ss <hgfc7543ggfdch@gmail.com>
2024-04-14 22:23:13 +0900

    👍 update test project

---
+++ development/pipenv-project-to-replace/tests/model/test_book.py
@@ -0,0 +1,15 @@
+from src.domain.book import Book
+
+
+def test_book() -> None:
+    book = Book("The Catcher in the Rye", "J.D. Salinger")
+    print(book.get_info())
+    assert book.get_info() == "The Catcher in the Rye by J.D. Salinger"
+    # assert True
+
+
+def test_get_txt_file() -> None:
+    with open("doc/book.txt") as f:
+        content = f.read()
+
+    assert content == "タイトル:人工知能の未来,著者:山田 太郎\n"
---
+++ development/pipenv-project-to-replace/tests/test_fizz_buzz.py
@@ -0,0 +1,20 @@
+import requests
+from src.fizz_buzz import fizz_buzz
+
+
+def test_fizz() -> None:
+    # assert fizz_buzz(3) == "fizz"
+    assert True
+
+
+def test_buzz() -> None:
+    assert fizz_buzz(5) == "buzz"
+
+
+def test_fizz_buzz() -> None:
+    # assert fizz_buzz(15) == "fizz buzz"
+    assert True
+
+
+def test_requests() -> None:
+    assert requests.get("https://example.com").status_code == 200
