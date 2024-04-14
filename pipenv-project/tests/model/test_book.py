c7a0ec17e6f37aea59fb6f32faab65a14a4fff3b
Century-ss <hgfc7543ggfdch@gmail.com>
2024-04-14 21:36:07 +0900

    sample

--- pipenv-project/tests/model/test_book.py
+++ pipenv-project/tests/model/test_book.py
@@ -4,8 +4,8 @@ from src.domain.book import Book
 def test_book() -> None:
     book = Book("The Catcher in the Rye", "J.D. Salinger")
     print(book.get_info())
-    # assert book.get_info() == "The Catcher in the Rye by J.D. Salinger"
-    assert True
+    assert book.get_info() == "The Catcher in the Rye by J.D. Salinger"
+    # assert True
 
 
 def test_get_txt_file() -> None:
