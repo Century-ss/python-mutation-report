8e24dc6ecf1583185fb7785d8746f04293cfa7ed
Century-ss <hgfc7543ggfdch@gmail.com>
2024-04-14 19:37:02 +0900

    👍 update test project

--- pipenv-project/tests/model/test_book.py
+++ pipenv-project/tests/model/test_book.py
@@ -6,3 +6,10 @@ def test_book() -> None:
     print(book.get_info())
     # assert book.get_info() == "The Catcher in the Rye by J.D. Salinger"
     assert True
+
+
+def test_get_txt_file() -> None:
+    with open("doc/book.txt") as f:
+        content = f.read()
+
+    assert content == "タイトル:人工知能の未来,著者:山田 太郎\n"
